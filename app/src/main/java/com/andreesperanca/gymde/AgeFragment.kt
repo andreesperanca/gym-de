fbbf362873ee42e115d8ea67f95e212b0252e6ac
andreeesperanca <andreluizesperancacorreia@gmail.com>
2023-01-26 18:26:55 -0300

    feat: basic finish register fragmnt

---
+++ app/src/main/java/com/andreesperanca/gymde/AgeFragment.kt
@@ -0,0 +1,31 @@
+package com.andreesperanca.gymde
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.andreesperanca.gymde.databinding.FragmentAgeBinding
+
+class AgeFragment : Fragment() {
+
+    private val binding by lazy {
+        FragmentAgeBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAdvance.setOnClickListener {
+            findNavController().navigate(R.id.action_focusFragment_to_finishRegister)
+        }
+
+    }
+
+}
