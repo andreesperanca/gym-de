6a847817cf9787fba04490d9b79b441436f8f618
andreeesperanca <andreluizesperancacorreia@gmail.com>
2023-01-26 12:02:18 -0300

    feat: gender hight and weight fragment basic

--- app/src/main/java/com/andreesperanca/gymde/HeightFragment.kt
+++ app/src/main/java/com/andreesperanca/gymde/HeightFragment.kt
@@ -5,55 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.andreesperanca.gymde.databinding.FragmentHeightBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HeightFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HeightFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val binding by lazy {
+        FragmentHeightBinding.inflate(layoutInflater)
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_height, container, false)
-    }
+    ): View = binding.root
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HeightFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HeightFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnAdvance.setOnClickListener {
+            findNavController().navigate(R.id.action_heightFragment_to_weightFragment)
+        }
     }
 }
--- app/src/main/java/com/andreesperanca/gymde/RegisterActivity.kt
+++ app/src/main/java/com/andreesperanca/gymde/RegisterActivity.kt
@@ -2,10 +2,17 @@ package com.andreesperanca.gymde
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.andreesperanca.gymde.databinding.ActivityRegisterBinding
+import com.andreesperanca.gymde.databinding.FragmentGenderBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityRegisterBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
     }
 }
