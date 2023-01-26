63e7cda7e7fe48cc64e0ed1e3b8315378a612e03
andreeesperanca <andreluizesperancacorreia@gmail.com>
2023-01-26 19:52:17 -0300

    ref: refactor code gender fragment xml

--- app/src/main/java/com/andreesperanca/gymde/LoginFragment.kt
+++ app/src/main/java/com/andreesperanca/gymde/LoginFragment.kt
@@ -7,16 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.andreesperanca.gymde.databinding.FragmentLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
 
     private val binding by lazy {
