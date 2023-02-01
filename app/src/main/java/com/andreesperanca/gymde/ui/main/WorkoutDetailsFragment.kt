9ed74a9a666817603761dd5b573510b9048504c1
andreeesperanca <andreluizesperancacorreia@gmail.com>
2023-02-01 17:32:51 -0300

    ref: refactor code workout details

--- app/src/main/java/com/andreesperanca/gymde/ui/main/WorkoutDetailsFragment.kt
+++ app/src/main/java/com/andreesperanca/gymde/ui/main/WorkoutDetailsFragment.kt
@@ -1,14 +1,11 @@
 package com.andreesperanca.gymde.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.andreesperanca.gymde.R
@@ -16,98 +13,114 @@ import com.andreesperanca.gymde.adapters.ExerciseAdapter
 import com.andreesperanca.gymde.databinding.FragmentWorkoutDetailsBinding
 import com.andreesperanca.gymde.ui.main.viewmodels.WorkoutDetailsViewModel
 import com.andreesperanca.gymde.utils.Resource
+import com.andreesperanca.gymde.utils.extensions.isVisible
 import com.andreesperanca.gymde.utils.extensions.toastCreator
+import com.andreesperanca.gymde.utils.generics.BaseFragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.progressindicator.LinearProgressIndicator
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class WorkoutDetailsFragment : Fragment() {
+class WorkoutDetailsFragment : BaseFragment<
+        FragmentWorkoutDetailsBinding,
+        WorkoutDetailsViewModel
+        >(R.layout.fragment_workout_details) {
 
-    val args: WorkoutDetailsFragmentArgs by navArgs()
+    private val args: WorkoutDetailsFragmentArgs by navArgs()
 
-    private val binding by lazy {
-        FragmentWorkoutDetailsBinding.inflate(layoutInflater)
-    }
+    /** UI COMPONENTS **/
+    private lateinit var tbWorkoutDetails: Toolbar
+    private lateinit var rvExercises: RecyclerView
+    private lateinit var fabCreateExercise: FloatingActionButton
+    private lateinit var progressBarWorkoutDetails: LinearProgressIndicator
 
-    private val adapter by lazy {
-        ExerciseAdapter()
-    }
-
-    private val viewModel: WorkoutDetailsViewModel by viewModel()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return binding.root
-    }
-
+    private val adapter by lazy { ExerciseAdapter() }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.rvExercise.adapter = adapter
-        binding.rvExercise.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false )
-
+        setupExercisesRecyclerView()
+        setupClickListeners()
+        /** FETCH DATA **/
         viewModel.fetchExercises(args.workout.uid)
+    }
 
-        binding.tbWorkoutToolbar.apply {
+    private fun setupClickListeners() {
+        fabCreateExercise.setOnClickListener {
+            val action =
+                WorkoutDetailsFragmentDirections.actionWorkoutDetailsFragmentToNewExerciseFragment(
+                    args.workout
+                )
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun setupExercisesRecyclerView() {
+        binding.rvExercise.adapter = adapter
+        binding.rvExercise.layoutManager =
+            LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+    }
+
+    override fun setupToolbar() {
+        tbWorkoutDetails.apply {
             title = args.workout.name
-            setNavigationOnClickListener { it.findNavController().popBackStack() }
-            binding.tbWorkoutToolbar.apply {
-                setOnMenuItemClickListener { menuItem ->
-                    when (menuItem.itemId) {
-                        R.id.excludeWorkout -> {
-                            viewModel.excludeWorkout(args.workout)
-                            true
-                        }
-                        R.id.editWorkout -> {
-                            toastCreator("Edit workout")
-                            false
-                        }
-                        else -> {
-                            false
-                        }
+            setNavigationOnClickListener { findNavController().popBackStack() }
+            setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.excludeWorkout -> {
+                        viewModel.excludeWorkout(args.workout)
+                        true
+                    }
+                    else -> {
+                        false
                     }
                 }
             }
+
         }
+    }
 
-        binding.extendedFab.setOnClickListener {
-            val action =
-                WorkoutDetailsFragmentDirections.actionWorkoutDetailsFragmentToNewExerciseFragment(args.workout)
-            findNavController().navigate(action)
-        }
+    override fun setupViewModel() {
+        val viewModel: WorkoutDetailsViewModel by viewModel()
+        this.viewModel = viewModel
+    }
 
-        viewModel.excludeWorkout.observe(viewLifecycleOwner) {
+    override fun setupObservers() {
+        viewModel.excludeWorkout.observe(viewLifecycleOwner) { excludeWorkout ->
 
-            when (it) {
+            when (excludeWorkout) {
                 is Resource.Success -> {
+                    progressBarWorkoutDetails.isVisible(false)
                     findNavController().navigate(R.id.action_workoutDetailsFragment_to_myWorkoutsFragment)
                 }
                 is Resource.Loading -> {
-                    binding.pgProgressBarWorkoutDetails.visibility = View.VISIBLE
+                    progressBarWorkoutDetails.isVisible(true)
                 }
                 is Resource.Error -> {
-                    toastCreator(it.message.toString())
-                    binding.pgProgressBarWorkoutDetails.visibility = View.INVISIBLE
+                    toastCreator(excludeWorkout.message.toString())
+                    progressBarWorkoutDetails.isVisible(false)
                 }
             }
 
         }
-
-        viewModel.fetchExercises.observe(viewLifecycleOwner) {
-            when (it) {
+        viewModel.fetchExercises.observe(viewLifecycleOwner) { exerciseList ->
+            when (exerciseList) {
                 is Resource.Success -> {
-                    adapter.updateData(it.data)
-                    binding.pgProgressBarWorkoutDetails.visibility = View.INVISIBLE
+                    adapter.updateData(exerciseList.data)
+                    progressBarWorkoutDetails.isVisible(false)
                 }
                 is Resource.Loading -> {
-                    binding.pgProgressBarWorkoutDetails.visibility = View.VISIBLE
+                    progressBarWorkoutDetails.isVisible(true)
                 }
                 is Resource.Error -> {
-                    toastCreator(it.message.toString())
-                    binding.pgProgressBarWorkoutDetails.visibility = View.INVISIBLE
+                    toastCreator(exerciseList.message.toString())
+                    progressBarWorkoutDetails.isVisible(false)
                 }
             }
         }
+    }
 
+    override fun initComponents() {
+        progressBarWorkoutDetails = binding.pgProgressBarWorkoutDetails
+        rvExercises = binding.rvExercise
+        fabCreateExercise = binding.fabCreateExercise
+        tbWorkoutDetails = binding.tbWorkoutToolbar
     }
 }
